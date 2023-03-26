ca800422a1634e77bfcc0c918c2bc8879aef6ea3
soue <soue@ya.ru>
2023-03-26 16:28:49 +0500

    продолжаем 3

--- main.py
+++ main.py
@@ -6,7 +6,7 @@ from keyboard import kb_client
 from aiogram.dispatcher.filters.state import State, StatesGroup
 from aiogram.contrib.fsm_storage.memory import MemoryStorage
 import config
-
+import mybase
 
 storage = MemoryStorage()
 bot = Bot(config.BotToken)
@@ -16,6 +16,7 @@ dp = Dispatcher(bot, storage=MemoryStorage())
 async def on_startup(_):
     print('Готов к работе! Данные обновлены')
     await bot.send_message(317076591, "Готов к работе! Данные обновлены")
+    await mybase.open_db()
 
 
 @dp.message_handler(commands=['start', 'help'])
@@ -28,33 +29,42 @@ async def commands_start(message: types.Message) -> None:
     await message.delete()
 
 
+# add 33вава33 111111 000000
 @dp.message_handler(filters.Text(startswith='add', ignore_case=True))
 async def search_by_number(message: types.Message):
-    print(message.text)
-    await message.reply('Можно добавлять номер и пароль')
+    pair = message.text.split()[1:3]
+    if pair[0].isdigit() and pair[0].isdigit():
+        try:
+            mybase.add_to_bd(pair)
+        except:
+            await message.reply('данные не сохранены, обратитесь к разработчику')
+        finally:
+            await message.reply('данные добавлены')
+    else:
+        await message.reply('ошибка во вводе данных')
 
 
 @dp.message_handler(lambda message: not message.text.isdigit())
 async def search_by_number(message: types.Message):
-    print(message.text)
-    await message.reply('Вы ввели не правильный номер')
+    await message.reply('Вы ввели неправильный номер')
 
 
 @dp.message_handler(lambda message: message.text.isdigit())
 async def search_by_number(message: types.Message):
-    print(message.text)
-    await message.reply('Вы ввели правильный номер')
+    passw = mybase.search_by_number(message.text)
+    if passw:
+        for qw in passw:
+            await message.reply(
+                f'№ ПУ <b>{qw[0]} </b>\n'
+                f'Пароль <b>{qw[1]} </b>\n', parse_mode='HTML')
+    else:
+        await message.reply(f'Пароль для прибора учета <b>{message.text}</b> отсутствует', parse_mode='HTML')
 
 
 async def on_shutdown(_):
     await bot.send_message(317076591, "Бот отключился, надо что то делать")
+    mybase.close_db()
 
-# , on_startup=on_startup, on_shutdown=on_shutdown
+
 if __name__ == '__main__':
-    executor.start_polling(dp, skip_updates=True)
-
-
-
-
-
-
+    executor.start_polling(dp, skip_updates=True, on_startup=on_startup, on_shutdown=on_shutdown)
---
+++ mybase.py
@@ -0,0 +1,46 @@
+import sqlite3
+
+global connect, cursor
+
+
+async def open_db():
+    global connect, cursor
+    connect = sqlite3.connect('sepass.sqlite3')
+    cursor = connect.cursor()
+    cursor.execute('''SELECT count(name) FROM sqlite_master WHERE type='table' AND name='pass' ''')
+    if cursor.fetchone()[0] == 1:
+        print('База данных подключена')
+    else:
+        print('что то пошло не так')
+
+
+def close_db():
+    connect.commit()
+    connect.close()
+
+
+# async def create_db(base_name: str, spisok: list):
+#     # Создание таблицы базы данных, на основании полученного списка из парсинга эксель файла.
+#     # На входе имя базы данных и список загрузки
+#     connect = sqlite3.connect(base_name)
+#     cursor = connect.cursor()
+#     cursor.execute('CREATE TABLE IF NOT EXISTS dis (contract, counterparty, city, point, street, house, tp)')
+#     sqlite_insert_query = """INSERT INTO dis
+#                                  (contract, counterparty, city, point, street, house, tp)
+#                                  VALUES (?, ?, ?, ?, ?, ?, ?);"""
+#     cursor.executemany(sqlite_insert_query, spisok)
+#     connect.commit()
+#     connect.close()
+
+
+def search_by_number(number: str):
+    # Функция поиска по ноиеру прибора учета: str,
+    results = cursor.execute(f"SELECT * FROM pass WHERE number LIKE '%{number.lower()}%'").fetchall()
+    return results
+
+
+def add_to_bd(newpass: list):
+    # Добавляет новую пару №ПУ- Пароль в базу данных
+    cursor.execute("INSERT INTO pass VALUES (?, ?);",
+                   (newpass[0], newpass[1]))
+    connect.commit()
